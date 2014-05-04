3a50d1764918eddacbf53851cf7f8abebc6a0bcd
Matthew Hughes <me@matthewhughes.co.uk>
2014-05-04 18:01:57 +0100

    Changed DB

--- AceTraining/logic/headerBar.php
+++ AceTraining/logic/headerBar.php
@@ -3,7 +3,7 @@ session_start();
 
 		$logOut = '<a href="logout.php">Log Out</a>';
 		$register = '<a href="../register.php">Register</a>';
-		$editProfile = '<a href="../$updateUsers.php">Edit Profile</a>';
+		$editProfile = '<a href="../updateUsers.php">Edit Profile</a>';
 
 		if(isset($_SESSION['LoggedIn'])){
 			echo $logOut;
